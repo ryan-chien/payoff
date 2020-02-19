8c6cb94bfc18da0615c92b9fc3278538571706d8
rychien-official <rvc5634@psu.edu>
2020-02-19 09:26:18 -0500

    min pay off

--- source.py
+++ source.py
@@ -6,12 +6,12 @@ import numpy as np
 
 # Declare variables -------------------
 num_loans = 4   # 4 separate loans
-term_months = 36   # three-year loan term
-rate_yearly = np.array([0.01, 0.05, 0.10, 0.15], dtype="float")   # interest rate is fixed
+term_months = 120-40   # remaining months
+rate_yearly = np.array([0.0531, 0.631, 0.0584, 0.0684], dtype="float")   # interest rate is fixed
 rate_monthly = rate_yearly / 12
-decision_min = [200, 175, 150, 125]   # minimum payment per term
-principal_initial = [20000, 17500, 15000, 12500]   # starting principal at time 0
-budget = 3250   # maximum payment possible per time period
+decision_min = [86.12, 70.54, 186.60, 204.72]   # minimum payment per term
+principal_initial = [9871.59, 11040.71, 20879.70, 19940.74]   # starting principal at time 0
+budget = 1800   # maximum payment possible per time period
 
 # Declare solver ---------------------------
 solver = pywraplp.Solver("payoff", pywraplp.Solver.GLOP_LINEAR_PROGRAMMING)
@@ -60,12 +60,12 @@ constraint_budget = \
             sum(decision[0:, t]) <= budget) for t in range(0, term_months)])
 
 ## Montly payment must meet minimum
-constraint_min_payment = \
+""" constraint_min_payment = \
     np.array(
         [[solver.Add(
             decision[i,t] >= decision_min[i])
             for t in range(0, term_months)]
-            for i in range(0, num_loans)])
+            for i in range(0, num_loans)]) """
 
 # Solve ----------------------
 status = solver.Solve()
@@ -75,7 +75,8 @@ payments = np.array([sum(decision_solution[:,t]) for t in range(0, term_months)]
 print(payments)
 principal_solution = np.array([[principal[i,t].solution_value() for t in range(0, term_months)] for i in range(0, num_loans)])
 print(principal_solution)
-solver.Objective().Value()
+print("Optimal payoff interest cost = " + 
+    str(solver.Objective().Value() - sum(principal_initial))) # interest cost w/ optimal schedule
 
 # Lp file ----------------------
 solver_debug_file = solver.ExportModelAsLpFormat(False)
